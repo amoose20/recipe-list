bf5b27c476efc90d72726c64ffa9fb10553fb7c2
amoose20 <amussey3@gmail.com>
2023-02-24 11:38:43 -0600

    services section completed

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,14 +1,29 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is a test', 'https://www.rubiconline.com/wp-content/uploads/2022/11/px650820-image-kwvv9jjg.jpg')
+        new Recipe(
+            'A Test Recipe', 
+            'This is a test', 
+            'https://www.rubiconline.com/wp-content/uploads/2022/11/px650820-image-kwvv9jjg.jpg',
+            [new Ingredient('test ingredients', 4)])
     ];
+
+    constructor(private slService: ShoppingListService) {
+
+    }
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }

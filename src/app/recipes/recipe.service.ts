6dbe25b72f64182185281b97e7c31fb77a261010
amoose20 <amussey3@gmail.com>
2023-02-24 10:31:22 -0600

    recipe service added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is a test', 'https://www.rubiconline.com/wp-content/uploads/2022/11/px650820-image-kwvv9jjg.jpg')
     ];
